de14dd484aaa4e060bfc05ee91bde248560396a1
PHILIPABDULRAZAK <philipabdulrazak1@gmail.com>
2023-07-13 12:27:51 -0700

    WILL WIN

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - A program that primts a line with enters function
- * Return: 0 (Success)
+ * main - A program that prints a line with enters function
+ * Return: 0. (Success)
  */
-int main(Void)
+int main(void)
 {
 enters("\"Programming is like building a multilingual puzzle");
 return (0);
